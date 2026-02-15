d2491c356c8be526b3e9c37001d2e441d9ea91ae
Irdan3440 <irdanramadhani@gmail.com>
2026-02-15 13:59:57 +0800

    model dan database

--- cek_mp.py
+++
@@ -1,9 +0,0 @@
-import mediapipe as mp
-try:
-    print(f"Path Mediapipe: {mp.__file__}")
-    pose = mp.solutions.pose
-    print("SUKSES: mp.solutions.pose ditemukan!")
-except AttributeError:
-    print("GAGAL: mp.solutions.pose TIDAK ditemukan. Masih ada konflik file/instalasi.")
-except Exception as e:
-    print(f"ERROR Lain: {e}")
